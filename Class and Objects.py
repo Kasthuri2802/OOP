7dd7265b9302baae7d9d5aad373d72023a5143fc
Kasthuri2802 <kasthurimuthuraj2002@gmail.com>
2023-12-05 21:11:36 +0530

    update

--- Class and Objects.py
+++ Class and Objects.py
@@ -1,6 +1,8 @@
 # Definition of Class:
 #     --> Class is a blueprint that defines some properties and behaviors
 #     --> Class is a code template for creating objects
+#     --> A class is not allocated memory when it is defined
+#     --> Class is a logical entity
 
 # Base class
 
@@ -21,4 +23,26 @@ class dog(animal):
 class cat(animal):
     def make_sound(self):
         print("I can make meow meow sound")
+
+# Definition of object: 
+#     --> Object is an instance of a class that has those properties and behaviours attached
+#     --> An object is allocated memory when it is created
+#     --> Objects are physical entities
+
+# Creating Object
+
+dog1 = dog()
+dog1.eat()
+dog1.sleep()
+dog1.make_sound()
+
+cat1 = cat()
+cat1.eat()
+cat1.sleep()
+cat1.make_sound()
+
+rabbit1 = animal()
+rabbit1.eat()
+rabbit1.sleep()
+rabbit1.make_sound()
 
